31e372255328ad08dac9dcfe2c69605eaf234e88
Amanpreet <apreet81@live.com>
2020-01-06 20:29:52 +0530

    Add project files.

---
+++ HangFireExample/App_Start/OwinStartup.cs
@@ -0,0 +1,36 @@
+﻿using Hangfire;
+using Owin;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+
+namespace HangFireExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //GlobalConfiguration.Configuration
+            //    .UseSqlServerStorage("Data Source=.;Initial Catalog=HangFireExample;User ID=sa;Password=12345");
+            GlobalConfiguration.Configuration
+                    .UseSqlServerStorage("Data Source=self-dzone.database.windows.net;Initial Catalog=test;User ID=Amanpreet;Password=Mentis123");
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            RecurringJob.AddOrUpdate(() => ExecuteWebhookRequest(), "*/10 * * * *");
+        }
+
+        public void ExecuteWebhookRequest()
+        {
+            var client = new RestClient("http://webhook.dzone.in/6709d976");
+            var request = new RestRequest(Method.POST);
+            string currentDateTime = DateTime.Now.ToString();
+            request.AddJsonBody(currentDateTime + "From Godaddy hosting");
+            IRestResponse response = client.Execute(request);
+        }
+    }
+}
---
+++ HangFireExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HangFireExample.MvcApplication" Language="C#" %>
